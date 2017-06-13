0567e1a8c1a06887a7a20344cf7765f1555b0557
nhat <minhnhat93@gmail.com>
2017-06-13 15:33:44 -0600

    update test_all.sh

--- test_all.sh
+++ test_all.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 DEVICE_ID=0
 CUDA_VISIBLE_DEVICE=${DEVICE_ID} ./experiments/scripts/gram_test.sh gpu 0 VGGnet_test M-30 | tee ~/faster-rcnn-M-30.log
+rm data/GRAM-RTM/annotations_cache/annots.pkl
 CUDA_VISIBLE_DEVICE=${DEVICE_ID} ./experiments/scripts/gram_test.sh gpu 0 VGGnet_test M-30-HD | tee ~/faster-rcnn-M-30-HD.log
+rm data/GRAM-RTM/annotations_cache/annots.pkl
 CUDA_VISIBLE_DEVICE=${DEVICE_ID} ./experiments/scripts/gram_test.sh gpu 0 VGGnet_test Urban1 | tee ~/faster-rcnn-Urban1.log
+rm data/GRAM-RTM/annotations_cache/annots.pkl
